da5945b0c6b3c78ba33a7c887318c3610a136cdd
lydia.dal@live.fr <lydia.dal@live.fr>
2018-10-12 10:55:53 +0200

    initial push test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Coucou la famille
           </a>
         </header>
       </div>
